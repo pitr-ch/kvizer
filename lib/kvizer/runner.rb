7d9eb8d83fb5d76fd6aa7f27eab4a0f3349de799
Marek Hulan <mhulan@redhat.com>
2013-01-14 09:17:28 +0100

    Job name fix

--- lib/kvizer/runner.rb
+++ lib/kvizer/runner.rb
@@ -194,7 +194,7 @@ class Kvizer
 
     def rebuild(vm_name, start_job_name, finish_job_name, collection_name = :base_jobs, job_options = { })
       collection = Kvizer::Jobs::Collection.new_by_names kvizer, *kvizer.config.send(collection_name)
-      job = collection[start_job_name] rescue Trollop::die(:job, "could not find job with name '#{start_job_name}'")
+      job = collection[start_job_name] rescue Trollop::die(:start_job, "could not find job with name '#{start_job_name}'")
 
       last_job = collection[finish_job_name] rescue nil
       if last_job.nil? && finish_job_name
