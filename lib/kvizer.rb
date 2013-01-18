c76ac7f57a116d9e279f840afdd3dbfeaf35fb2e
Petr Chalupa <pchalupa@redhat.com>
2013-01-18 13:47:38 +0100

    better error message when ambiguous vm name

--- lib/kvizer.rb
+++ lib/kvizer.rb
@@ -37,10 +37,10 @@ class Kvizer
 
   def vm(part_name)
     vms.find { |vm| vm.name == part_name } || begin
-      regexp = part_name.kind_of?(String) ? /#{part_name}/ : part_name
-      vms.select { |vm| vm.name =~ regexp }.
-          tap { |arr| raise "ambiguous vm name #{part_name}" if arr.size > 1 }.
-          first
+      regexp     = part_name.kind_of?(String) ? /#{part_name}/ : part_name
+      candidates = vms.select { |vm| vm.name =~ regexp }
+      raise "ambiguous vm name '#{part_name}' candidates: #{candidates.map(&:name).join ', '}" if candidates.size > 1
+      candidates.first
     end
   end
 
